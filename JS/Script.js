c27177e13782e2a11bbd62055e9088d4578f01c2
Rinaldo Nelson <rinaldo.nelson1@gmail.com>
2022-09-14 12:05:49 -0400

    Typo Fix

---
+++ JS/Script.js
@@ -0,0 +1,47 @@
+class Calculator {
+    constructor(previousOperandtextElement, currentOperandtextElement) {
+        this.previousOperandtextElement = previousOperandtextElement
+        this.currentOperandtextElement = currentOperandtextElement
+    }
+
+    clear() {
+        this.currentOperand = ''
+        this.prevOperand = ''
+        this.operation = undefined
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOpertaion(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-equals]')
+const allClearButton = document.querySelector('[data-equals]')
+const previousOperandtextElement = document.querySelector('[data-previous-operand]')
+const currentOperandtextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandtextElement, currentOperandtextElement)
+
+numberButtons2.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+
+});
